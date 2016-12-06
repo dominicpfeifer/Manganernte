ebdf1957a2b534115b37f6ec459fb849ae03736f
Dominic Pfeifer <d.pfeifer@novum-gemueseabo.de>
2016-12-06 15:52:00 +0100

    Leerzeile hinzugefügt

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
  * Created by dominicpfeifer on 05.12.16.
  */
 public class HelloWorld {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
